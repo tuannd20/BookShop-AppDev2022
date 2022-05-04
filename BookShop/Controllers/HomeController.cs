a6024230cec8e7e1b899e7680a3448706d6665ee
Tuan Duy <tuan.nguyenduy@smartdev.com>
2022-05-04 22:41:42 +0700

    Profile page for customer and seller

--- BookShop/Controllers/HomeController.cs
+++ BookShop/Controllers/HomeController.cs
@@ -81,7 +81,7 @@ namespace BookShop.Controllers
         }
          public IActionResult Profile()
         {
-            return View();
+            return View("Views/Home/Profile.cshtml");
         }
 
         public async Task<IActionResult> AddToCart(string isbn)
